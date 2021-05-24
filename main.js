4b01f7e5d586c043274feb18577bac99c69f6b86
Pierre BAUSIERE <pierre.bausiere@bell.ca>
2021-05-24 00:23:21 -0400

    <feat> remove gui and change name

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-import * as dat from 'dat.gui';
 import * as THREE from 'THREE';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { Sky } from 'three/examples/jsm/objects/Sky';
@@ -44,16 +43,12 @@ scene.add(gridHelper);
 const controls = new OrbitControls(camera, renderer.domElement);
 
 // Sky
-
 function initSky (){
   const sky = new Sky();
   sky.scale.setScalar(450000)
   scene.add(sky);
 
   const sun = new THREE.Vector3();
-
-  /// GUI
-
   const effectController = {
     turbidity: 10,
     rayleigh: 3,
@@ -63,39 +58,21 @@ function initSky (){
     azimuth: 180,
     exposure: renderer.toneMappingExposure
   };
+  const uniforms = sky.material.uniforms;
+  uniforms[ 'turbidity' ].value = effectController.turbidity;
+  uniforms[ 'rayleigh' ].value = effectController.rayleigh;
+  uniforms[ 'mieCoefficient' ].value = effectController.mieCoefficient;
+  uniforms[ 'mieDirectionalG' ].value = effectController.mieDirectionalG;
 
-  function guiChanged() {
+  const phi = THREE.MathUtils.degToRad( 90 - effectController.elevation );
+  const theta = THREE.MathUtils.degToRad( effectController.azimuth );
 
-    const uniforms = sky.material.uniforms;
-    uniforms[ 'turbidity' ].value = effectController.turbidity;
-    uniforms[ 'rayleigh' ].value = effectController.rayleigh;
-    uniforms[ 'mieCoefficient' ].value = effectController.mieCoefficient;
-    uniforms[ 'mieDirectionalG' ].value = effectController.mieDirectionalG;
+  sun.setFromSphericalCoords( 1, phi, theta );
 
-    const phi = THREE.MathUtils.degToRad( 90 - effectController.elevation );
-    const theta = THREE.MathUtils.degToRad( effectController.azimuth );
+  uniforms[ 'sunPosition' ].value.copy( sun );
 
-    sun.setFromSphericalCoords( 1, phi, theta );
-
-    uniforms[ 'sunPosition' ].value.copy( sun );
-
-    renderer.toneMappingExposure = effectController.exposure;
-    renderer.render( scene, camera );
-
-  }
-
-  const gui = new dat.GUI();
-
-  gui.add( effectController, 'turbidity', 0.0, 20.0, 0.1 ).onChange( guiChanged );
-  gui.add( effectController, 'rayleigh', 0.0, 4, 0.001 ).onChange( guiChanged );
-  gui.add( effectController, 'mieCoefficient', 0.0, 0.1, 0.001 ).onChange( guiChanged );
-  gui.add( effectController, 'mieDirectionalG', 0.0, 1, 0.001 ).onChange( guiChanged );
-  gui.add( effectController, 'elevation', 0, 90, 0.1 ).onChange( guiChanged );
-  gui.add( effectController, 'azimuth', - 180, 180, 0.1 ).onChange( guiChanged );
-  gui.add( effectController, 'exposure', 0, 1, 0.0001 ).onChange( guiChanged );
-
-  guiChanged();
-
+  renderer.toneMappingExposure = effectController.exposure;
+  renderer.render( scene, camera );
 }
 initSky();
 
